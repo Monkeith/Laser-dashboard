f4068e53af9a0be42762f44effdc2e820a7ba89f
Monkeith <116558833+Monkeith@users.noreply.github.com>
2024-01-31 00:12:49 +0100

    Functies toegevoegd.

--- app.py
+++ app.py
@@ -4,60 +4,130 @@ import dash_bootstrap_components as dbc
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import time
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
+SAMPLE_RATE = 44100
+SAMPLE_TIME = 1/SAMPLE_RATE
 
-sidebar = html.Div([
-    dbc.Button(id="open-offcanvas", n_clicks=0),
-    dbc.Offcanvas(
-        children=[
-            dbc.Label("Voer gewenste frequentie in:"),
-            dbc.Input(
-                id= 'frequency-input',
-                type='number',
-                value=330,
-                min=1,
-                max=20000,
-                step=1,
-                placeholder='Frequentie'
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+
+def drawFigure():
+    return  html.Div([
+        dbc.Card(
+            dbc.CardBody([
+                dcc.Graph(
+                    figure=px.bar(
+                        df, x="sepal_width", y="sepal_length", color="species"
+                    ).update_layout(
+                        template='plotly_dark',
+                        plot_bgcolor= 'rgba(0, 0, 0, 0)',
+                        paper_bgcolor= 'rgba(0, 0, 0, 0)',
+                    ),
+                    config={
+                        'displayModeBar': False
+                    }
+                ) 
+            ])
+        ),  
+    ])
+
+# Text field
+def drawText():
+    return html.Div([
+        dbc.Card(
+            dbc.CardBody([
+                html.Div([
+                    html.H2("Text"),
+                ], style={'textAlign': 'center'}) 
+            ])
+        ),
+    ])
+
+# Data
+df = px.data.iris()
+
+app.layout = html.Div([
+     dbc.Card(
+         dbc.CardBody([
+             dbc.Row(
+                 html.H1('Lasermicrofoon', style={'fontsize': 40, 'textAlign':'center'})
+             ),
+             html.Hr(),
+             dbc.Row([
+                dbc.Col([
+                    html.Div([
+                        dcc.Graph(id='sine-wave'),
+                        dcc.Interval(
+                            id='update-interval',
+                            interval=100,
+                            n_intervals=0
+                        )
+                    ])
+                ], width=3),
+                dbc.Col([
+                    drawFigure()
+                ], width=3 ),
+                dbc.Col([
+                    drawFigure()
+                ], width=3),
+                dbc.Col([
+                    html.Div([
+                    dcc.Dropdown(['FIR','IIR','Bandpass'], 'FIR', id='dropdown', clearable=False),
+                    ]),
+                ]),
+             ]),
+             html.Hr(),
+             
+             dbc.Row([
+                 dbc.Col([
+                     drawFigure()
+                 ], width=6),
+                 dbc.Col([
+                    drawFigure()
+                 ], width=6),
+             ]),
+         ]),
+     ),
+    html.Div([
+        dbc.Button(id="open-offcanvas", n_clicks=0),
+        dbc.Offcanvas(
+            children=[
+                dbc.Label("Kies gewenste frequentie"),
+               html.Div([
+                    dcc.RadioItems(
+                        id='radios',
+                        className='btn-group',
+                        inputClassName='btn-group',
+                        labelClassName='btn btn-outline-primary',
+                        options=[
+                            {'label': '220 Hz', 'value': 200},
+                            {'label': '330 Hz', 'value': 330},
+                            {'label': '460 Hz', 'value': 460},
+                            {'label': '590 Hz', 'value': 590},
+                            {'label': '720 Hz', 'value': 720},
+                            {'label': '850 Hz', 'value': 850},
+                            {'label': '980 Hz', 'value': 980},
+                            {'label': '1110 Hz', 'value': 1110},
+                            {'label': '1240 Hz', 'value': 1240},
+                            {'label': '1370 Hz', 'value': 1370},
+                            {'label': '1500 Hz', 'value': 1500},
+                            {'label': '1630 Hz', 'value': 1630},
+                            {'label': '1760 Hz', 'value': 1760},
+                            {'label': '1890 Hz', 'value': 1890},
+                            {'label': '2020 Hz', 'value': 2020},
+                        ],
+                        value=1,
+                    ),
+                    html.Div(id='Output'),
+                ],
+                    className='radio-group',
                 ),
-
-            dbc.ListGroup([
-                dbc.ListGroupItem("300Hz", id="300Hz", n_clicks=0, action=True),
-                dbc.ListGroupItem("400Hz", id="400Hz", n_clicks=0, action=True),
-                dbc.ListGroupItem("500Hz", id="500Hz", n_clicks=0, action=True),
-                dbc.ListGroupItem("600Hz", id="600Hz", n_clicks=0, action=True),
-                dbc.ListGroupItem("700Hz", id="700Hz", n_clicks=0, action=True),
-                dbc.ListGroupItem("800Hz", id="800Hz", n_clicks=0, action=True),
-            ]),
-        ],
-        id="offcanvas",
-        title="Geluiden",
-        is_open=False,
-    ),
-])
-
-content = html.Div([
-    dbc.Col(
-        html.Div(), width=6
-    ),
-    dbc.Col(
-        dbc.DropdownMenu([
-            dbc.DropdownMenuItem('FIR'),
-            dbc.DropdownMenuItem('IIR'),
-            dbc.DropdownMenuItem('...'),
-        ])
-    ),
-])
-app.layout = dbc.Container([
-    dbc.Row([
-        dbc.Col(html.H1("Lasermicrofoon", style={'fontSize': 40, 'textAlign': 'center'}))
+            ],
+            id="offcanvas",
+            title="Geluiden",
+            is_open=False,
+        ),  
     ]),
-
-    html.Hr(),  # Horizontal line
-
-    content,sidebar,
-    dcc.Graph(id='wave-plot')
 ])
 
 @app.callback(
@@ -70,6 +140,39 @@ def toggle_offcanvas(n1, is_open):
         return not is_open
     return is_open
 
+@app.callback(
+    Output('filter-type','children'),
+    Input('dropdown','value')
+)
+
+def update_filter(value):
+    return f'You have selected {value}'
+
+@app.callback(
+    Output('sine-wave', 'figure'),
+    Input('update-interval', 'n_intervals'),
+    Input('radios', 'value')
+)
+def update_sine_wave(n, frequency):
+    t_values = np.arange(0,0.1,SAMPLE_TIME)
+
+    current_time = time.time()
+    sine_wave = np.sin(2 * np.pi * frequency * (t_values - current_time))
+
+    figure = {
+        'data': [
+            {'x': t_values, 'y': sine_wave, 'type': 'line', 'name': 'Sine Wave'},
+        ],
+        'layout': {
+            'title': f'Real-Time Sine Wave (Frequency: {frequency} Hz)',
+            'xaxis': {'title': 'Time'},
+            'yaxis': {'title': 'Amplitude'},
+            'plot_bgcolor':'#000000',
+            'paper_bgcolor':'#83939A'
+        }
+    }
+
+    return figure
 
 if __name__ == "__main__":
     app.run_server(debug=False)
