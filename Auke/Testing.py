44013024eff971082daead6ccebd68fcdcf2cf8e
Auke de Haan <a.a.de.haan@st.hanze.nl>
2024-01-30 16:00:01 +0100

    Toevoegen van WebsocketForRasberryPy.py

--- Auke/Testing.py
+++ Auke/Testing.py
@@ -1,86 +1,135 @@
+import dash
+from dash import State, Input, Output, html, dcc
+import dash_bootstrap_components as dbc
 import psycopg2
 import mysecrets
-import plotly.graph_objs as go
 
-class MultiBuffer:
-    def __init__(self, buffer_size, num_subbuffers):
-        self.buffer_size = buffer_size
-        self.num_subbuffers = num_subbuffers
-        self.buffers = [[] for _ in range(num_subbuffers)]
+update_frequency = 200
 
-    def add_data(self, data):
-        # Voeg nieuwe gegevens toe aan de eerste subbuffer
-        self.buffers[0].append(data)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
 
-        # Als de eerste subbuffer te groot is, schuif deze door naar de volgende subbuffer
-        if len(self.buffers[0]) >= self.buffer_size:
-            for i in range(self.num_subbuffers - 1):
-                self.buffers[i], self.buffers[i + 1] = self.buffers[i + 1], self.buffers[i]
-            self.buffers[-1] = []
 
-    def get_data(self):
-        # Geef de gegevens van de laatste subbuffer terug
-        return self.buffers[-1]
-
-    def read_bytearrays_from_database(self, table_name, column_name, num_records):
-        try:
-            # Verbinding maken met de database
-            conn = psycopg2.connect(
-                dbname=mysecrets.DATABASE_NAME,
-                user=mysecrets.DATABASE_USER,
-                password=mysecrets.DATABASE_PASSWORD,
-                host=mysecrets.DATABASE_HOST,
-                port=mysecrets.DATABASE_PORT,
-            )
-
-            # Maak een cursor object
-            cur = conn.cursor()
-
-            # Voer een query uit om de byte-array gegevens uit de tabel te lezen
-            cur.execute(f"SELECT {column_name} FROM {table_name} LIMIT {num_records}")
-
-            # Haal de byte-array gegevens op en voeg ze toe aan de buffer
-            byte_arrays = cur.fetchall()
-            for byte_array in byte_arrays:
-                self.add_data(byte_array[0])
-
-        except psycopg2.Error as e:
-            print("Error:", e)
-
-        finally:
-            # Sluit de cursor en de verbinding
-            if cur:
-                cur.close()
-            if conn:
-                conn.close()
-
-# Maak een MultiBuffer-object en lees bytearrays uit de database
-buffer_size = 100  # Grootte van elke subbuffer
-num_subbuffers = 3  # Aantal subbuffers
-multi_buffer = MultiBuffer(buffer_size, num_subbuffers)
-multi_buffer.read_bytearrays_from_database("bytearray_table", "byte_array", 10)
-
-# Haal de bytearrays op uit de buffer
-byte_array_values = multi_buffer.get_data()
-
-# Maak een lijngrafiek met behulp van Plotly
-def create_line_chart(byte_array_values):
-    return go.Figure(
-        data=[
-            go.Scatter(
-                x=list(range(len(byte_array_values))),
-                y=byte_array_values,
-                mode='lines',
-                name='Byte Array'
-            )
-        ],
-        layout=go.Layout(
-            title='Byte Array Grafiek',
-            xaxis={'title': 'Index'},
-            yaxis={'title': 'Waarde'}
+# Functie om de frequentie op te halen uit de database
+def get_latest_frequency():
+    try:
+        # Maak verbinding met de TimescaleDB-database
+        conn = psycopg2.connect(
+            dbname=mysecrets.DATABASE_NAME,
+            user=mysecrets.DATABASE_USER,
+            password=mysecrets.DATABASE_PASSWORD,
+            host=mysecrets.DATABASE_HOST,
+            port=mysecrets.DATABASE_PORT,
         )
-    )
+        cursor = conn.cursor()
 
-# Weergave van de grafiek
-fig = create_line_chart(byte_array_values)
-fig.show()
+        # Haal de meest recente frequentie op uit de database
+        cursor.execute("SELECT frequentie FROM audio LIMIT 1")
+
+        latest_frequency_row = cursor.fetchone()
+        if latest_frequency_row is not None:
+            latest_frequency = latest_frequency_row[0]
+        else:
+            latest_frequency = "No data available"
+
+        conn.close()
+
+        return latest_frequency
+
+    except Exception as e:
+        print("Error:", e)
+        return "Error fetching data"
+
+
+# Definieer de functie drawFigure
+def drawFigure():
+    return html.Div([
+        dcc.Graph(
+            figure={'data': [{'x': [1, 2, 3], 'y': [4, 4, 1], 'type': 'bar', 'name': 'SF'}],
+                    'layout': {'title': 'Dash Data Visualization'}}
+        )
+    ])
+
+
+# Definieer de lay-out van het dashboard
+app.layout = html.Div([
+    dbc.Card(
+        dbc.CardBody([
+            dbc.Row(
+                html.H1('Lasermicrofoon', style={'fontsize': 40, 'textAlign': 'center'})
+            ),
+            html.Hr(),
+            dbc.Row([
+                dbc.Col([
+                    drawFigure()
+                ], width=3),
+                dbc.Col([
+                    drawFigure()
+                ], width=3),
+                dbc.Col([
+                    drawFigure()
+                ], width=3),
+                dbc.Col([
+                    dbc.DropdownMenu([
+                        dbc.DropdownMenuItem('FIR'),
+                        dbc.DropdownMenuItem('IIR'),
+                        dbc.DropdownMenuItem('...'),
+                    ])
+                ])
+            ]),
+            html.Hr(),
+
+            dbc.Row([
+                dbc.Col([
+                    drawFigure()
+                ], width=6),
+                dbc.Col([
+                    drawFigure()
+                ], width=6),
+            ]),
+        ]),
+    ),
+    html.Div([
+        dbc.Button(id="open-offcanvas", n_clicks=0),
+        dbc.Offcanvas(
+            children=[
+                dbc.Label("Voer gewenste frequentie in:"),
+                dbc.Input(
+                    id='frequency-input',
+                    type='number',
+                    value=get_latest_frequency(),
+                    min=1,
+                    max=20000,
+                    step=1,
+                    placeholder='Frequentie'
+                ),
+
+                dbc.ListGroup([
+                    dbc.ListGroupItem("300Hz", id="300Hz", n_clicks=0, action=True),
+                    dbc.ListGroupItem("400Hz", id="400Hz", n_clicks=0, action=True),
+                    dbc.ListGroupItem("500Hz", id="500Hz", n_clicks=0, action=True),
+                    dbc.ListGroupItem("600Hz", id="600Hz", n_clicks=0, action=True),
+                    dbc.ListGroupItem("700Hz", id="700Hz", n_clicks=0, action=True),
+                    dbc.ListGroupItem("800Hz", id="800Hz", n_clicks=0, action=True),
+                ]),
+            ],
+            id="offcanvas",
+            title="Geluiden",
+            is_open=False,
+        ),
+    ])
+])
+
+
+@app.callback(
+    Output("offcanvas", "is_open"),
+    Input("open-offcanvas", "n_clicks"),
+    [State("offcanvas", "is_open")],
+)
+def toggle_offcanvas(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
+
+
+if __name__ == "__main__":
+    app.run_server(debug=False)
